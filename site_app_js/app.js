ac15f3f7ade2a70e0a70cfdd33e83f9a7143d5c1
myrdall <trivarson@gmail.com>
2024-03-26 21:34:01 +0700

    init 2
    Добавление новых файлов проекта, изменение расширений файлов, структуры проекта.

--- site_app_js/app.js
+++ site_app_js/app.js
@@ -2,18 +2,25 @@ const express = require('express');
 const sequelize = require('sequelize');
 const mysql = require('mysql2');
 
-
+const { SRV_PORT, SRV_HOST, DB_HOST, DB_USER, DB_PASSWORD, DB_NAME} = require('./config');
 
 const app = express();
-const hostname = '127.0.0.1';
-const port = 3000;
 
+app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/public/index.html');
+    res.render('index');
 });
 
-app.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`);
+app.get('/booking', (req, res) => {
+    res.render('booking');
+});
+
+app.use((req, res, next) => {
+    res.status(404).render('oops');
+});
+
+app.listen(SRV_PORT, SRV_HOST, () => {
+    console.log(`Server running at http://${SRV_HOST}:${SRV_PORT}/`);
 });
---
+++ site_app_js/config.js
@@ -0,0 +1,17 @@
+const SRV_PORT = 3000;
+const SRV_HOST = 'localhost';
+
+const DB_HOST = 'localhost';
+const DB_USER = 'root';
+const DB_PASSWORD = 'password';
+const DB_NAME = 'mydatabase';
+
+module.exports = {
+
+    SRV_PORT,
+    SRV_HOST,
+    DB_HOST,
+    DB_USER,
+    DB_PASSWORD,
+    DB_NAME
+};
